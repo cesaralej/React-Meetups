cb954cc2ead5ecf8623aa9ab91e9aa7ab7227387
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2023-06-11 17:25:51 +0200

    Clickable modal

---
+++ src/components/Backdrop.js
@@ -0,0 +1,4 @@
+function Backdrop(props) {
+    return <div className="backdrop" onClick={props.onClose} />;
+}
+export default Backdrop;
---
+++ src/components/Modal.js
@@ -0,0 +1,22 @@
+function Modal(props) {
+    function cancelHandler() {
+        props.onClose();
+    }
+
+    function confirmHandler() {
+        props.onConfirm();
+    }
+
+    return (
+        <div className="modal">
+            <p>Are you sure?</p>
+            <div className="modal-content">
+                <button className='btn btn--alt' onClick={cancelHandler}>Cancel</button>
+                <button className="btn" onClick={confirmHandler}>
+                    Confirm
+                </button>
+            </div>
+        </div>
+    );
+}
+export default Modal;
